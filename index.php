b046af1dd45da6a996e15643474f1322a4af382e
kateemcg <88216309+katemcginness@users.noreply.github.com>
2021-08-02 11:06:07 +1000

    making a php file
    first update for php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Back end web development - week 1</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+        <?php echo "<h1>Week 1</h1>" ?>
+        <?php echo "<p>Making a php site</p>" ?>
+    </body>
+</html>
